70293235f666c3b6bbc1c7a698041c8a01530adf
草鞋没号 <308487730@qq.com>
2022-02-18 17:15:57 +0800

    比例调整

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   "plugins": {
     "autoprefixer": {},
     // https://github.com/vitejs/vite/issues/3046
-    "postcss-pxtorem": { rootValue: 75, unitPrecision: 5, propList: ['*'] },
+    "postcss-pxtorem": { rootValue: 100, unitPrecision: 5, propList: ['*'] },
   },
 };
